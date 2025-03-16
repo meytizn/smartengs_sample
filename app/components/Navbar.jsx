1ccbde9cb2f31672fb5b6e51f2e55e86b577f438
meytizn <zamanianmehdi23desktop@gmail.com>
2025-03-16 17:51:43 +0330

    navbar bug fixed

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 
 export default function navbar() {
 let [dlogo,setDlogo]=useState(" m15 19-7-7 7-7")
-let [margin,setMarging]= useState("mx-[-500px]")
+let [margin,setMarging]= useState("left-[600px]")
 let [mobile,setMobile]=useState(false)
 
 let navlists=[
@@ -19,17 +19,17 @@ let navlists=[
   setMobile(!mobile)
   if(mobile==true){
     return(
-    setMarging("mx-[0px]"),
+    setMarging("left-[60px]"),
      setDlogo("m9 5 7 7-7 7"))
 
   }
-  return (setMarging("mx-[-500px]"),setDlogo("m15 19-7-7 7-7") )
+  return (setMarging("left-[600px]"),setDlogo("m15 19-7-7 7-7") )
 
 }
 
   return (
     <>
-    <div className="relative flex flex-row justify-around content-center items-center  w-[100%] h-[60px] bg-red-800 text-[25px]  text-white">
+    <div className="relative overflow-hidden flex flex-row justify-around content-center items-center  w-[100%] h-[60px] bg-red-800 text-[25px]  text-white">
       
       <div className=""> SmartEngs </div>
 
@@ -57,17 +57,25 @@ let navlists=[
 
 
 
+
+{/* movile navbar */}
+<div className={`md:hidden fixed top-[60px] bottom-0 ${margin} flex flex-col  items-center gap-8 p-10 float-right w-[100%] text-center  text-white text-[25px] bg-red-800 my-1  transition-all duration-400 `}> 
+       
+       {navlists.map((list,index)=>(<div key={index}>{list.title}</div>))}
+         
+         </div>
+
+
+
+
+
+
       </div>
 
 
 
 
-{/* movile navbar */}
-      <div className={`md:hidden flex flex-col  items-center gap-8 p-10 float-right w-[100%] text-center  text-white text-[25px] bg-red-800 my-1 ${margin} transition-all duration-400 `}> 
-       
-      {navlists.map((list,index)=>(<div key={index}>{list.title}</div>))}
-        
-        </div>
+
 
       </>
   )
