f060d1a0207401b7cab0e1672b778af2097d1dc0
meytizn <zamanianmehdi23desktop@gmail.com>
2025-03-17 00:21:37 +0330

    products components created successfully

---
+++ app/components/LatestProducts.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { v4 as uuidv4 } from 'uuid';
+import Image from 'next/image'
+
+
+
+
+export default function LatestProducts() {
+  let products=[
+    {id:uuidv4(),title:"tv",price:3000,image: "/images/tv.jpg", description:"samsung tv is the best"},
+    {id:uuidv4(),title:"xbox",price:8000,image: "/images/tv.jpg", description:"xbox is the best"},
+    {id:uuidv4(),title:"Iphone16",price:12000,image:"/images/tv.jpg" , description:"Iphone16 is the best"},
+    {id:uuidv4(),title:"Laptop",price:9000,image:"/images/tv.jpg" , description:"Laptop is the best"},
+    
+  ]
+
+  
+  return (
+    <>
+    <div className='bg-red-800 text-white my-1 flex flex-row justify-center items-center content-center w-[100%] h-[50px] text-center text-[20px]'>Latest Products</div>
+    
+    <div className='bg-red-800 p-5 text-white flex flex-row justify-around gap-5 md:gap-2  md:justify-center items-center content-center flex-wrap w-[100%]  text-center text-[20px]'>
+      
+      {products.map((product,index)=>(
+        <div  className='w-[45%] md:w-[20%] bg-white text-black rounded-md  ' key={index}>
+        <div className='w-[100%]'><Image className='m-auto rounded-md' src={product.image} width={200} height={200} alt={product.title}/></div>
+        <div className='w-[100%] text-center'>{product.title}</div>
+
+        <div className='flex flex-row justify-start w-[100%] text-center'>
+        <div className='bg-blue-500 w-[50%] '>{product.price}</div>
+        <div  className='bg-green-300 text-white text-[20px] w-[50%] '><button>
+          
+        <svg className="   text-white dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="none" viewBox="0 0 24 24">
+  <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="M5 4h1.5L9 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-8.5-3h9.25L19 7H7.312"/>
+</svg>
+
+          
+          </button></div>
+        </div>
+
+
+        </div>
+        
+        ))}
+
+
+
+      </div>
+    
+   
+    </>
+  )
+}
--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,12 @@
 
 import Image from "next/image";
 import Navbar from "./components/Navbar";
+import LatestProducts from "./components/LatestProducts";
 export default function Home() {
 
   return (
     <>
-      
+      <LatestProducts/>
       </>
   )
 }
